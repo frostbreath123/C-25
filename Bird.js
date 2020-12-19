5b80fcf4e240620ea5222cf94915a46777032f4c
frostbreath123 <75172886+frostbreath123@users.noreply.github.com>
2020-12-19 20:55:23 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,13 @@
+//extends for extending parent class
+//super= for constructor (changing)
+class Bird extends BaseClass{
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+  }
+  display(){
+    this.body.position.x = mouseX;
+    this.body.position.y = mouseY;
+    super.display();
+  }
+}
